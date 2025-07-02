91f46a8d6950953fe558c37ee5b9372ea375cbc0
adorable-otter <plan.stack@gmail.com>
2025-07-02 22:56:47 +0900

    프로그래머스 - 주식 가격 (스택)

---
+++ stockPrice.js
@@ -0,0 +1,28 @@
+function solution(prices) {
+  const answer = Array(prices.length).fill(1);
+  const stack = []; // 가격이 떨어지지 않은 시간대의 index를 저장
+
+  for (let i = 0; i < prices.length - 1; i++) {
+    const currPrice = prices[i];
+
+    while (stack.length > 0 && currPrice < prices[stack.at(-1)]) {
+      stack.pop();
+    }
+    stack.forEach((i) => {
+      answer[i]++;
+    });
+    stack.push(i);
+  }
+  answer[answer.length - 1] = 0;
+  return answer;
+}
+
+const prices = [1, 2, 3, 2, 3];
+
+console.log(solution(prices));
+
+/**
+ * 가격이 떨어지지 않았다 == 현재 가격이 이전 가격보다 크다
+ * 한 번 떨어지면 더 이상 체크할 필요 없음
+ * 떨어지지 않은 것들을 스택에 넣는다.
+ */
